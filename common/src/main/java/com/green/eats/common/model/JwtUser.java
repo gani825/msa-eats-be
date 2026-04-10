0195df95c2a6d66b48c978fb7cc34558528bdc5a
gani825 <kgee0503@gmail.com>
2026-04-10 13:16:30 +0900

    fix(common): JwtUser name 필드 및 toString 추가

--- common/src/main/java/com/green/eats/common/model/JwtUser.java
+++ common/src/main/java/com/green/eats/common/model/JwtUser.java
@@ -2,11 +2,13 @@ package com.green.eats.common.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 // JWT를 만들 때 payload에 담을 객체
 // 최소한의 정보만 담는 것이 보안상 좋음
 @Getter
 @AllArgsConstructor
+@ToString
 public class JwtUser {
     private long signedUserId; // 로그인한 유저의 PK
     private String name;
