60c33b5afc9848422cc6de14b3fd25ff5341bd52
gani825 <kgee0503@gmail.com>
2026-04-16 15:01:52 +0900

    feat(order): UserCacheRepository, OrderRepository 추가

--- order-service/src/main/java/com/green/eats/order/application/OrderRepository.java
+++ order-service/src/main/java/com/green/eats/order/application/OrderRepository.java
@@ -3,5 +3,6 @@ package com.green.eats.order.application;
 import com.green.eats.order.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// Order 엔티티 전용 JPA Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 }
--- order-service/src/main/java/com/green/eats/order/application/UserCacheRepository.java
+++ order-service/src/main/java/com/green/eats/order/application/UserCacheRepository.java
@@ -3,6 +3,6 @@ package com.green.eats.order.application;
 import com.green.eats.order.entity.UserCache;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// UserCache 엔티티 전용 JPA Repository
 public interface UserCacheRepository extends JpaRepository<UserCache, Long> {
-
 }
