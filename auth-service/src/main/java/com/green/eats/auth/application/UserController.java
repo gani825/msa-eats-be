d8b7803787a2ad37fc9c2d83afe6d631283e90c7
gani825 <kgee0503@gmail.com>
2026-04-17 11:17:44 +0900

    feat(auth): 유저 정보 수정 API 추가 (PUT /user)

--- auth-service/src/main/java/com/green/eats/auth/application/UserController.java
+++ auth-service/src/main/java/com/green/eats/auth/application/UserController.java
@@ -1,19 +1,19 @@
 package com.green.eats.auth.application;
 
+import com.green.eats.auth.application.model.UserPutSigninReq;
 import com.green.eats.auth.application.model.UserSigninRes;
 import com.green.eats.auth.application.model.UserSigninReq;
 import com.green.eats.auth.application.model.UserSignupReq;
 import com.green.eats.auth.entity.User;
+import com.green.eats.common.auth.UserContext;
 import com.green.eats.common.model.JwtUser;
 import com.green.eats.common.model.ResultResponse;
+import com.green.eats.common.model.UserDto;
 import com.green.eats.common.security.JwtTokenManager;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -52,6 +52,17 @@ public class UserController {
         return ResultResponse.builder()
                 .resultMessage("로그인 성공")
                 .resultData(userSigninRes)
+                .build();
+    }
+
+    @PutMapping
+    public ResultResponse<?> update(@RequestBody UserPutSigninReq req) {
+        // signin() 때 issue()로 담아둔 AT 쿠키에서 유저 정보 꺼내기
+        UserDto userDto = UserContext.get();
+        userService.update(userDto.id(), req);
+        return ResultResponse.builder()
+                .resultMessage("수정 완료")
+                .resultData(1)
                 .build();
     }
 }
