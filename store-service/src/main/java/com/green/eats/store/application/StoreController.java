dfa211cc5c60f58684a6de3d7736c28cd5061a07
gani825 <kgee0503@gmail.com>
2026-04-20 11:18:01 +0900

    feat(store): StoreController 추가 (메뉴 등록, 목록 조회 API)

--- store-service/src/main/java/com/green/eats/store/application/StoreController.java
+++ store-service/src/main/java/com/green/eats/store/application/StoreController.java
@@ -4,9 +4,13 @@ import com.green.eats.common.auth.UserContext;
 import com.green.eats.common.model.ResultResponse;
 import com.green.eats.common.model.UserDto;
 import com.green.eats.store.application.model.MenuGetRes;
+import com.green.eats.store.application.model.MenuPostReq;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,6 +20,18 @@ import java.util.List;
 @RequiredArgsConstructor
 public class StoreController {
     private final StoreService storeService;
+
+    @PostMapping("/menu")
+    public ResultResponse<?> addMenu(@Valid @RequestBody MenuPostReq req) {
+        log.info("menuPostReq: {}", req);
+
+        // MenuPostReq를 받아서 메뉴 등록 처리
+        storeService.addMenu(req);
+
+        return ResultResponse.builder()
+                .resultMessage("success")
+                .build();
+    }
 
     @GetMapping("/menu")
     public ResultResponse<?> getAllMenus() {
