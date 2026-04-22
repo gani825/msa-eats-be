3f7a79422c1ab2f525ddea0466c9578ba9f54a22
gani825 <kgee0503@gmail.com>
2026-04-22 09:52:07 +0900

    feat(order): GET /api/order 엔드포인트 추가

--- order-service/src/main/java/com/green/eats/order/application/OrderController.java
+++ order-service/src/main/java/com/green/eats/order/application/OrderController.java
@@ -4,11 +4,15 @@ import com.green.eats.common.auth.UserContext;
 import com.green.eats.common.model.ResultResponse;
 import com.green.eats.common.model.UserDto;
 import com.green.eats.order.application.model.OrderPostReq;
+import com.green.eats.order.entity.Order;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -25,6 +29,17 @@ public class OrderController {
         return ResultResponse.builder()
                 .resultMessage("success")
                 .resultData(orderId)
+                .build();
+    }
+
+    @GetMapping
+    public ResultResponse<?> getMyOrders() {
+        // Gateway가 헤더로 넘긴 유저 정보에서 userId 꺼내기
+        UserDto userDto = UserContext.get();
+        List<Order> orders = orderService.getMyOrders(userDto.id());
+        return ResultResponse.builder()
+                .resultMessage(String.format("%d rows", orders.size()))
+                .resultData(orders)
                 .build();
     }
 }
