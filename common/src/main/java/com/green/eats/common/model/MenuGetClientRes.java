be0a983c5d66fb607ea63f0dc018d60944ed3d3c
gani825 <kgee0503@gmail.com>
2026-04-22 09:52:57 +0900

    feat(common): MenuGetClientRes DTO 추가 - order-service에서 메뉴 정보 조회용

--- common/src/main/java/com/green/eats/common/model/MenuGetClientRes.java
+++ common/src/main/java/com/green/eats/common/model/MenuGetClientRes.java
@@ -1,4 +1,17 @@
 package com.green.eats.common.model;
 
+import lombok.*;
+
+/**
+ order-service에서 store-service의 메뉴 정보를 FeignClient로 조회할 때 사용하는 응답 DTO
+ - 주문 상세 조회 시 menuId → 메뉴명 매핑을 위해 사용됨
+ */
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MenuGetClientRes {
+    private Long menuId;
+    private String name;
+    private Integer price;
 }
